871a9b71516646afaa018dcc9111914fa3f128d9
SteffenKoehler <steffen-koehler1@gmx.de>
2019-11-19 18:19:14 +0100

    🔥 cleanup lib files

--- projects/ngx-boolean-input/src/public-api.ts
+++ projects/ngx-boolean-input/src/public-api.ts
@@ -2,6 +2,3 @@
  * Public API Surface of ngx-boolean-input
  */
 
-export * from './lib/ngx-boolean-input.service';
-export * from './lib/ngx-boolean-input.component';
-export * from './lib/ngx-boolean-input.module';
