5782c98e1964595077130d1eab3054931bf737a4
SteffenKoehler <steffen-koehler1@gmx.de>
2021-09-16 14:31:45 +0200

    ✅  update test to remove console errors

--- apps/showcase/src/app/app.component.spec.ts
+++ apps/showcase/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { EditableComponent } from './editable/editable.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, EditableComponent],
     }).compileComponents();
   });
 
